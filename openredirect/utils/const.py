269d0baccf293877bf5acc0aa064f1c7154debad
Karthikeyan C <114332111+cyber-karthi@users.noreply.github.com>
2024-06-21 13:41:17 +0530

    Update const.py

--- openredirect/utils/const.py
+++ openredirect/utils/const.py
@@ -12,7 +12,7 @@
 
 
 class Data:
-    blog = 'https://blogs.cappriciosec.com/blog/137/Open Redirect'
+    blog = 'https://blogs.cappriciosec.com/blog/143/Open Redirect'
     api = 'https://api.cappriciosec.com/Telegram/cappriciosecbot.php'
     config_path = '~/.config/cappriciosec-tools/cappriciosec.yaml'
     payloadurl = 'https://raw.githubusercontent.com/Cappricio-Securities/PayloadAllTheThings/main/openredirect.txt'
